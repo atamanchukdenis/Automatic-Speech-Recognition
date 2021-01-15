64d3da3953c6a36f1f13ad4128693e0375a7185d
Dzianis Atamanchuck <dzianis.atamanchuck1@ibm.com>
2021-01-16 00:42:02 +0300

    Added run script with hardcoded dataset paths

---
+++ run.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+import automatic_speech_recognition as asr
+
+dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/en/train_trunc_dataset.csv', batch_size=32)
+dev_dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/en/val_trunc_dataset.csv', batch_size=32)
+alphabet = asr.text.Alphabet(lang='en')
+features_extractor = asr.features.FilterBanks(
+    features_num=160,
+    winlen=0.02,
+    winstep=0.01,
+    winfunc=np.hanning
+)
+model = asr.model.get_deepspeech(
+    input_dim=160,
+    output_dim=29
+)
+optimizer = tf.optimizers.Adam(
+    lr=1e-4,
+    beta_1=0.9,
+    beta_2=0.999,
+    epsilon=1e-8
+)
+decoder = asr.decoder.GreedyDecoder()
+pipeline = asr.pipeline.CTCPipeline(
+    alphabet, features_extractor, model, optimizer, decoder
+)
+pipeline.fit(dataset, dev_dataset, epochs=25)
+pipeline.save('/checkpoint')
+
+test_dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/en/val_trunc_dataset.csv')
+wer, cer = asr.evaluate.calculate_error_rates(pipeline, test_dataset)
+print(f'WER: {wer}   CER: {cer}')
