c8a7c9327ca758ab35a0c4803cd6d786b6bc7351
Dzianis Atamanchuck <dzianis.atamanchuck1@ibm.com>
2021-01-16 17:47:25 +0300

    Reduced batch size to 48 for ru training to fit in memory

--- run-ru.py
+++ run-ru.py
@@ -6,8 +6,8 @@ import automatic_speech_recognition as asr
 #running only on GTX 1080 until RTX 2060 is fixed
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
-dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/ru/cv-corpus-6.0-2020-12-11/ru/train_dataset.csv', batch_size=64)
-dev_dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/ru/cv-corpus-6.0-2020-12-11/ru/val_dataset.csv', batch_size=64)
+dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/ru/cv-corpus-6.0-2020-12-11/ru/train_dataset.csv', batch_size=48)
+dev_dataset = asr.dataset.Audio.from_csv('/home/datamanc/data/CommonVoice/ru/cv-corpus-6.0-2020-12-11/ru/val_dataset.csv', batch_size=48)
 alphabet = asr.text.Alphabet(lang='ru')
 features_extractor = asr.features.FilterBanks(
     features_num=160,
